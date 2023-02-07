b71445058abdca7a20ea69ef94e88f3248b86fbc
Marco Aurélio Delgado <110215723+Delgado75@users.noreply.github.com>
2023-02-06 21:58:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function mostraBio(id){
+    if (document.getElementById(id).style.display == "none"){
+        document.getElementById(id).style= "display:block;";
+    }
+    else{
+    document.getElementById(id).style= "display:none;";
+    }
+}
+
+/*if (document.getElementById(id).style.display == "none"){
+    document.getElementById(id).style= "display:block;";
+}
+else{
+document.getElementById(id).style= "display:none;";
+}*/
